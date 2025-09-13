33d70290651db80ce3e9070ad013794428669f8b
yunfeng <3557558991@qq.com>
2025-09-13 11:49:18 +0800

    笔记相关代码

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -12,7 +12,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.78.78:6379");
+        config.useSingleServer().setAddress("redis://192.168.185.78:6379");
         return Redisson.create(config);
     }
 }
--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,20 +1,124 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("笔记不存在");
+        }
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+        isLikeBlog(blog);
+        return Result.ok(blog);
+    }
+
+    @Override
+    public Result likeBlog(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        String key = "blog:like:" + id;
+        Double score = redisTemplate.opsForZSet().score(key, userId);
+        //未点赞
+        if (score == null) {
+            boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
+            if (isSuccess) {
+                redisTemplate.opsForZSet().add(key, userId, System.currentTimeMillis());
+            }
+        } else {
+            //已点赞
+            boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
+            if (isSuccess) {
+                redisTemplate.opsForZSet().remove(key, userId);
+            }
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result queryBlogLikes(Long id) {
+        String key = "blog:like:" + id;
+        Set<Object> top5 = redisTemplate.opsForZSet().range(key, 0, 4);
+        if (top5 == null || top5.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<Long> ids = top5.stream()
+                .map(o -> {
+                    if (o instanceof Long) return (Long) o;
+                    if (o instanceof Integer) return ((Integer) o).longValue();
+                    return Long.valueOf(o.toString());
+                })
+                .collect(Collectors.toList());
+        String idStr = StrUtil.join(",", ids);
+        List<UserDTO> userDTOS = userService.query()
+                .in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(userDTOS);
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(blog -> {
+            Long userId = blog.getUserId();
+            User user = userService.getById(userId);
+            blog.setName(user.getNickName());
+            blog.setIcon(user.getIcon());
+            isLikeBlog(blog);
+        });
+        return Result.ok(records);
+    }
+
+    @Override
+    public void isLikeBlog(Blog blog) {
+        UserDTO user = UserHolder.getUser();
+        if (user == null) {
+            return;
+        }
+        Long userId = user.getId();
+        String key = "blog:like:" + blog.getId();
+        Double score = redisTemplate.opsForZSet().score(key, userId);
+        if (score != null) {
+            blog.setIsLike(true);
+        }
+    }
 }
